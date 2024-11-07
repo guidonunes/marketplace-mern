d0bac96bdbe104f067e5406250df0b3c770e1944
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-07 18:39:05 -0300

    set up public routes

---
+++ dashboard/src/router/routes/publicRoutes.js
@@ -0,0 +1,13 @@
+import Login from '../../views/auth/Login';
+import Register from '../../views/auth/Register';
+
+const publicRoutes = [
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/register',
+    element: <Register/>
+  }
+]
