7661ce19982ca732cd7b7eed9f15a2d98f756e59
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-11 15:29:06 -0300

    update p tag text

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -6,11 +6,11 @@ const Register = () => {
       <div className="w-[350px] text-[#ffffff] p-2">
         <div className="bg-[#6f68d1] p-4 rounded-md">
           <h2 className='text-x1 mb-3 font-bold'>Welcome to the E-Commerce Page </h2>
-          <p className='text-sm mb-3 font-medium'>Sign up</p>
+          <p className='text-sm mb-3 font-medium'>Please, register your account</p>
           <form>
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" placeholder='name' className='p-2 rounded-md bg-[#ffffff]' />
+              <input type="text" id="name" name="name" placeholder='name' className='p-2 rounded-md bg-[#ffffff]' required />
             </div>
           </form>
         </div>
