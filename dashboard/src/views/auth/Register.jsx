15e0ecf39cd59ab2f82192d83faf0ad6747098d9
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-11 16:10:06 -0300

    add input fields to form

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -8,9 +8,20 @@ const Register = () => {
           <h2 className='text-x1 mb-3 font-bold'>Welcome to the E-Commerce Page </h2>
           <p className='text-sm mb-3 font-medium'>Please, register your account : </p>
           <form>
+            {/* Add name field */}
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="name">Name</label>
               <input type="text" id="name" name="name" placeholder='Enter your name' className='p-2 rounded-md bg-[#ffffff]' required />
+            </div>
+            {/* Add email field */}
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="email">Email</label>
+              <input type="text" id="email" name="email" placeholder='Enter your Email' className='p-2 rounded-md bg-[#ffffff]' required />
+            </div>
+            {/* Add password field */}
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Password</label>
+              <input type="text" id="password" name="password" placeholder='Password' className='p-2 rounded-md bg-[#ffffff]' required />
             </div>
           </form>
         </div>
