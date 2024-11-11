706df72c882cb140fbbe0faf648bcc1b839686d1
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-11 15:27:23 -0300

    add label and input field for name

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -7,6 +7,12 @@ const Register = () => {
         <div className="bg-[#6f68d1] p-4 rounded-md">
           <h2 className='text-x1 mb-3 font-bold'>Welcome to the E-Commerce Page </h2>
           <p className='text-sm mb-3 font-medium'>Sign up</p>
+          <form>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" placeholder='name' className='p-2 rounded-md bg-[#ffffff]' />
+            </div>
+          </form>
         </div>
       </div>
     </div>
