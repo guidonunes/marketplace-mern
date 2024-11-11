c3be66047cbc9952b9db4e5ddc861db634cf5c28
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-11 17:08:45 -0300

    add facebook login icon

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaGoogle } from "react-icons/fa";
+import { FaGoogle, FaFacebookF } from "react-icons/fa";
 
 const Register = () => {
   return (
@@ -45,6 +45,9 @@ const Register = () => {
             <div className="flex justify-center items-center gap-3">
               <div className="w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden">
                 <span><FaGoogle /></span>
+              </div>
+              <div className="w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden">
+                <span><FaFacebookF /></span>
               </div>
             </div>
           </form>
