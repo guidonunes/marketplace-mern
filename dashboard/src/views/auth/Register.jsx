5ddfbe2eebfdd800a3af03b584a54ed00d89969b
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-11 15:15:23 -0300

    add styling to divs and header

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Register = () => {
   return (
     <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
+      <div className="w-[350px] text-[#ffffff] p-2">
+        <div className="bg-[#6f68d1] p-4 rounded-md">
+          <h2 className='text-x1 mb-3 font-bold'>Welcome to the E-Commerce Page </h2>
 
+        </div>
+      </div>
     </div>
   );
 };
