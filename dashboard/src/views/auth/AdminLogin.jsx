b38eb036271e081912b6932e5f572ef6b9cca275
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-12 11:03:44 -0300

    add logo to admin page

--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -26,9 +26,11 @@ const AdminLogin = () => {
     <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
       <div className="w-[350px] text-[#ffffff] p-2">
         <div className="bg-[#6f68d1] p-4 rounded-md">
-          <h2 className='text-x1 mb-3 font-bold'>Welcome to the E-Commerce Page </h2>
-          <p className='text-sm mb-3 font-medium'>Please, enter your email and password : </p>
-
+          <div className="">
+            <div className="">
+              <img src="/images/logo.png" alt="logo" />
+            </div>
+          </div>
           <form onSubmit={submitForm}>
 
             {/* Add email field */}
