4d7fa5a3ff01aa905d6639da9425698afdeac178
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-13 09:29:51 -0300

    implement initial redux toolkit setup

---
+++ dashboard/src/store/Reducers/authReducer.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+expect const authReducer = createSlice({
+  name: "auth",
+  initialState: {
+    successMessage: "",
+    errorMessage: "",
+    loader: false,
+    userInfo: ''
+  },
+
+  reducers: {
+
+  },
+  extraReducers: {
+
+  }
+
+});
+
+export default authReducer.reducer;
