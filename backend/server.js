517ec4e50d0b6bddcaed1bb0bc33c2a160255632
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-13 11:33:00 -0300

    set up basic backend config

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config();
 
 
+app.get('/', (req, res) => res.send('Hello World!'));
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
