0727bf6300227a0aad4982d75e105ba0075a3705
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-14 09:59:20 -0300

    implement method in the component

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,4 +3,4 @@ const authControllers = require('../controllers/authControllers');
 
 const router = require('express').Router();
 
-router.post('/admin-login', authControllers)
+router.post('/admin-login', authControllers.admin_login)
