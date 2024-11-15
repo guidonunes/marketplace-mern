d78d3496b743a6b34711dbdc8220b578ef8d0e4d
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-15 10:34:21 -0300

    add missing forward slash to string

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,16 @@ const cookieParser = require('cookie-parser');
 app.use(cors({
   origin: ['http://localhost:3000'],
   credentials: true
-}))
+}));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-
-app.use('api', require('./routes/authRoutes'));
+// Add the correct path for the route
+app.use('/api', require('./routes/authRoutes'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
