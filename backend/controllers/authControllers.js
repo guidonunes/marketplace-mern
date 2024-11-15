a9056702cca75c38fabd0edec4abe1e1b05f31a9
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-15 11:46:04 -0300

    call adminModel function into the try block

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,8 +1,11 @@
+const adminModel = require('../models/adminModel');
+
 class authControllers {
   admin_login = async(req, res) => {
     const { email, password } = req.body;
     try {
-
+      const admin = await adminModel.findOne({email}).select('+password');
+      console.log(admin);
     } catch (error) {
 
     }
