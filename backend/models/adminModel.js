ffbf3d17340366192317ef1e0ae17f7da3003c90
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-15 11:38:56 -0300

    add data to Schema object

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -1,1 +1,17 @@
 const {Schema, model} = require('mongoose');
+
+
+const adminSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
