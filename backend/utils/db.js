1c8e483efce90d6af360a0ee8392fc4d10a15cfa
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-15 11:02:45 -0300

    log error message to the console

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -6,6 +6,6 @@ module.exports.dbConnect = async () => {
     await mogoose.connect(process.env.DB_URL,{useNewUrlParser: true})
     console.log('Database connected')
   } catch (error) {
-
+    console.log('Database connection failed')
   }
 }
