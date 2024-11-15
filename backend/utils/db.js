67487951a6a8b587f5316226ee8e869fe2cb691e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-15 11:01:52 -0300

    add try/catch block

---
+++ backend/utils/db.js
@@ -0,0 +1,11 @@
+const mogoose = require('mongoose');
+
+
+module.exports.dbConnect = async () => {
+  try {
+    await mogoose.connect(process.env.DB_URL,{useNewUrlParser: true})
+    console.log('Database connected')
+  } catch (error) {
+
+  }
+}
