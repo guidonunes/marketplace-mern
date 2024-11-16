6663b5ad75190caa8a5524689fdf53e8dc7376eb
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-16 16:44:42 -0300

    implement jsonWebToken logic for signing in

--- backend/utils/tokeCreate.js
+++ backend/utils/tokeCreate.js
@@ -1,4 +1,6 @@
 const jsonWebToken = require('jsonwebtoken');
 module.exports.createToken = async(data) =>{
-  const token = await jsonWebToken.sign(data)
+  const token = await jsonWebToken.sign(data, process.env.SECRET_KEY, {
+    expiresIn: '1h'});
+    return token;
 }
