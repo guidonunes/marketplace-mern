1fe2464e05d62a688b3a4dd4538075dc49a6d9f4
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-20 17:50:46 -0300

    add new router for getUser

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,7 @@ const authControllers = require('../controllers/authControllers');
 const router = require('express').Router();
 
 router.post('/admin-login', authControllers.admin_login)
+router.get('/get-user', authControllers.getUser)
 
 
 module.exports = router;
