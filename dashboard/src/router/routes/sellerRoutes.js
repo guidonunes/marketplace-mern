46db97da3d8b67660c43b34a2e62afa1a25f844a
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-20 18:12:11 -0300

    add ability to routes

--- dashboard/src/router/routes/sellerRoutes.js
+++ dashboard/src/router/routes/sellerRoutes.js
@@ -5,6 +5,7 @@ const Home = lazy(() => import('../../views/pages/Home'));
 export const sellerRoutes = [
   {
     path: '/',
-    element: <Home />
+    element: <Home />,
+    ability: ['admin','seller']
   }
 ]
