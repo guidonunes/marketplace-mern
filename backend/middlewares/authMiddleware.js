76c50dc357abf6f7434d5a4ac56fe8bdea6fc3ba
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-20 17:48:35 -0300

    add try/catch block to component

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -5,8 +5,14 @@ module.exports.authMiddleware = async (req, res, next) => {
 
   if(!accessToken){
     return res.status(409).json({error: 'Unauthorized'});
-
   } else {
-
+    try {
+      const decoded = jwt.verify(accessToken, process.env.SECRET);
+      req.role = decoded.role;
+      req.id = decoded.id;
+      next()
+    } catch(error) {
+      return res.status(409).json({error: 'Unauthorized'});
+    }
   }
 }
