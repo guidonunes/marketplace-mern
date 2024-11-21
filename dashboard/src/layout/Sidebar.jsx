414b7de1b8b11e2b576796a8efe898e4b59d04da
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-21 17:03:48 -0300

    implement getNav method

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
+
+  const [allNav, setAllNav] = useState([])
+  useEffect(() => {
+    const navs = getNav('admin');
+
+  }, [])
+
   return (
     <div>
       <div></div>
       <div className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47/_5%)] transition-all`}>
         <div className='h-[70px] flex justify-center items-center'>
           <Link to='/' className='w-[180px] h-[50px]'>
-            <img src="/images/logo.png" alt="logo" />
+            <img className='' src="/images/logo.png" alt="logo" />
           </Link>
+        </div>
+        <div className="px-[16px]">
+
         </div>
       </div>
     </div>
---
+++ dashboard/src/navigation/allNav.js
@@ -0,0 +1,11 @@
+import { AiOutlineDashBoard } from 'react-icons/ai';
+
+export const allNav = [
+  {
+    id: 1,
+    title: 'Dashboard',
+    icon: <AiOutlineDashBoard/>,
+    role: 'admin',
+    path: '/admin/dashboard'
+  }
+]
