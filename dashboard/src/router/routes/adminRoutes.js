cd03646db449ff95cea4dcb7d8f083b6777f1460
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-21 14:54:04 -0300

    update role propertie

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -1,5 +1,4 @@
 import { lazy } from 'react';
-import AdminDashboard from '../../views/admin/AdminDashboard';
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'));
 
 
@@ -8,6 +7,6 @@ export const adminRoutes = [
   {
     path: 'admin/dashboard',
     element: <AdminDashboard />,
-    ability: ['admin', 'seller'],
+    role: 'admin'
   }
 ]
