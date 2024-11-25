ab2f576f7f28d52d23b322dca6f10e3bcb1dea89
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-25 17:06:37 -0300

    add span tag

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -36,6 +36,7 @@ const Sidebar = () => {
             <li>
               <button className='text-[#030811] font-bold duration-200 px-[12px] py-[8px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1'>
                 <span><RiLogoutCircleLine /></span>
+                <span>Logout</span>
               </button>
             </li>
            </ul>
