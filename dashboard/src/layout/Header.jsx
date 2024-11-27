fab7b99b2fa379892cf0059398b491ca75f80085
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-27 17:23:04 -0300

    set items prop to center position

--- dashboard/src/layout/Header.jsx
+++ dashboard/src/layout/Header.jsx
@@ -4,7 +4,7 @@ import { FaList } from 'react-icons/fa6';
 const Header = ({showSidebar, setShowSidebar}) => {
   return (
     <div className='fixed top-0 left-0 w-full py-5 pl-2 lg:px-8 z-40'>
-      <div className='ml-0 lg:ml-[260px] rounded-md h-[64px] flex justify-between items-start bg-[#b1addf] px-5 trasition-all'>
+      <div className='ml-0 lg:ml-[260px] rounded-md h-[64px] flex justify-between items-center bg-[#b1addf] px-5 trasition-all'>
         <div  onClick={() => {setShowSidebar(!showSidebar)}} className='w-[35px] flex lg:hidden h-[35px] rounded-sm bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 justify-center items-center cursor-pointer '>
           <span><FaList/></span>
         </div>
