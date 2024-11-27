9b8a48828e91bd452a06bfed0ba3c3e1a33e0302
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-27 15:18:22 -0300

    add tailwind styling to the header

--- dashboard/src/layout/Header.jsx
+++ dashboard/src/layout/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='fixed top-0 left-0 w-full py-5 pl-2 lg:px-8 z-40'>
+      <div className='ml-0 lg:ml-[260px] rounded-md h-[64px] flex justify-between items-start bg-[#b1addf] px-5 trasition-all'>
+
+      </div>
     </div>
   );
 };
