65c4f7d1d7f28c890e13642666fad579a8baca0b
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-27 15:21:22 -0300

    set up useState method

--- dashboard/src/layout/MainLayout.jsx
+++ dashboard/src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 const MainLayout = () => {
+  const  [showSidebar, setShowSidebar] = useState(false);
+
   return (
     <div className = 'bg-[#cdcae9] w-full min-h-screen'>
       <Header />
