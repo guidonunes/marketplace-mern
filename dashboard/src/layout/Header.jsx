9e54f9b6923285f11a8aa1f310cb4aa572b83942
Guilherme Nunes <guilherme.augd@gmail.com>
2024-11-27 15:29:15 -0300

    implement use state hook and add icon, plus styling

--- dashboard/src/layout/Header.jsx
+++ dashboard/src/layout/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { FaList } from 'react-icons/fa6';
 
-const Header = () => {
+const Header = ({showSidebar, setShowSidebar}) => {
   return (
     <div className='fixed top-0 left-0 w-full py-5 pl-2 lg:px-8 z-40'>
       <div className='ml-0 lg:ml-[260px] rounded-md h-[64px] flex justify-between items-start bg-[#b1addf] px-5 trasition-all'>
-
+        <div  onClick={() => {setShowSidebar(!showSidebar)}} className='w-[35px] flex lg:hidden'>
+          <span><FaList/></span>
+        </div>
       </div>
     </div>
   );
