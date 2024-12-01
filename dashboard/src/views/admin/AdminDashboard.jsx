63f32bffa9181f312ecd5662dd91fab3f6cb7690
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-01 18:08:56 -0300

    imported icon for products and update the style of the card

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { MdCurrencyExchange } from "react-icons/md";
+import { MdProductionQuantityLimits } from "react-icons/md";
 
 const AdminDashboard = () => {
   return (
     <div className='px-2 md:px-7 py-5'>
       <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7'>
+
         <div className='flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3'>
           <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
             <h2 className='text-3xl font-bold'>$3434</h2>
@@ -14,6 +16,37 @@ const AdminDashboard = () => {
             <MdCurrencyExchange className='text-[#fae8e8] shadow-lg' />
           </div>
         </div>
+
+        <div className='flex justify-between items-center p-5 bg-[#fde2ff] rounded-md gap-3'>
+          <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+            <h2 className='text-3xl font-bold'>50</h2>
+            <span className='text-md font-medium'>Products</span>
+          </div>
+          <div className='w-[40px] h-[48px] rounded-full bg-[#760077] flex justify-center items-center text-xl'>
+            <MdProductionQuantityLimits className='text-[#fae8e8] shadow-lg' />
+          </div>
+        </div>
+
+        <div className='flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3'>
+          <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+            <h2 className='text-3xl font-bold'>$3434</h2>
+            <span className='text-md font-medium'>Total Sales</span>
+          </div>
+          <div className='w-[40px] h-[48px] rounded-full bg-[#fa0305] flex justify-center items-center text-xl'>
+            <MdCurrencyExchange className='text-[#fae8e8] shadow-lg' />
+          </div>
+        </div>
+
+        <div className='flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3'>
+          <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+            <h2 className='text-3xl font-bold'>$3434</h2>
+            <span className='text-md font-medium'>Total Sales</span>
+          </div>
+          <div className='w-[40px] h-[48px] rounded-full bg-[#fa0305] flex justify-center items-center text-xl'>
+            <MdCurrencyExchange className='text-[#fae8e8] shadow-lg' />
+          </div>
+        </div>
+
       </div>
 
     </div>
