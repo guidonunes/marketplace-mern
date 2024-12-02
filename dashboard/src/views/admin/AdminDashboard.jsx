2725d5723e170a062eb6f13b9377ee377e2677cc
Guilherme Nunes <guilherme.augd@gmail.com>
2024-12-02 16:57:32 -0300

    replicate li tags in the message box

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -149,6 +149,33 @@ const AdminDashboard = () => {
                     </div>
                   </div>
                 </li>
+
+                <li className='mb-3 ml-6'>
+                  <div className='flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4c4fe2] rounded-full z-10'><img className='w-full rounded-full h-full shadow-lg' src="/images/admin.jpg" alt='admin img'/></div>
+                  <div className='p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm'>
+                    <div className='flex justify-between items-center mb-2'>
+                      <Link className='text-md font-normal text-[#d0d2d6]'>Doug The Boss</Link>
+                      <time className='mb-1 text-sm font-normal sm:order-last sm:mb-0'>2 days ago</time>
+                    </div>
+                    <div className='p-2 text-xs font-norma bg-slate-700 rounded lg border border-slate-800'>
+                      How are you?
+                    </div>
+                  </div>
+                </li>
+
+                <li className='mb-3 ml-6'>
+                  <div className='flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] bg-[#4c4fe2] rounded-full z-10'><img className='w-full rounded-full h-full shadow-lg' src="/images/admin.jpg" alt='admin img'/></div>
+                  <div className='p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm'>
+                    <div className='flex justify-between items-center mb-2'>
+                      <Link className='text-md font-normal text-[#d0d2d6]'>Doug The Boss</Link>
+                      <time className='mb-1 text-sm font-normal sm:order-last sm:mb-0'>2 days ago</time>
+                    </div>
+                    <div className='p-2 text-xs font-norma bg-slate-700 rounded lg border border-slate-800'>
+                      How are you?
+                    </div>
+                  </div>
+                </li>
+
               </ol>
             </div>
           </div>
